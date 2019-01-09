9aa7e12d1b447ced226bf2c478568175c924979a
Imad Youssoufi <imad.youssoufi@be.ey.com>
2019-01-09 14:43:24 +0100

    add KNN

--- knn-tf/index.js
+++ knn-tf/index.js
@@ -1,7 +1,24 @@
-require('@tensorflow/tfjs')
+require('@tensorflow/tfjs-node')
 const tf = require('@tensorflow/tfjs')
 const loadCSV = require('./load-csv')
 const dataFile = 'kc_house_data.csv'
+
+
+function knn(features, labels , predctionPoint , k){
+  return features
+    .sub(predctionPoint)
+    .pow(2)
+    .sum(1)
+    .pow(0.5)
+    .expandDims(1)
+    .concat(labels,1)
+    .unstack()
+    .sort((a,b) => a.get(0) > b.get(0) ? 1 : -1)
+    .slice(0,k)
+    .reduce((acc, pair) => acc + pair.get(1), 0) / k
+}
+
+
 
 let { features, labels, testFeatures, testLabels } = loadCSV(dataFile,{
     shuffle : true,
@@ -9,3 +26,5 @@ let { features, labels, testFeatures, testLabels } = loadCSV(dataFile,{
     dataColumns : ['lat','long'],
     labelColumns: ['price']
 })
+console.log(testLabels)
+console.log()
