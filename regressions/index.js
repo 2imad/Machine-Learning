5cc7ceb303ec5983ad53ff52b979275ab1eb0886
Imad Youssoufi <imadyoussoufi@gmail.com>
2019-01-10 16:42:17 +0100

    add TensorFlow

--- regressions/index.js
+++ regressions/index.js
@@ -2,6 +2,7 @@ require('@tensorflow/tfjs-node');
 const tf = require("@tensorflow/tfjs");
 const loadCSV = require("./load-csv")
 const CSVFile = './cars.csv'
+const LinearRegression = require("./linear-regression")
 
 
 let {features, labels, testFeatures, testLabels  } = loadCSV(CSVFile,{
@@ -11,4 +12,11 @@ let {features, labels, testFeatures, testLabels  } = loadCSV(CSVFile,{
     labelColumns:['mpg']   
 })
 
-console.log(features)
+const regression = new LinearRegression(features, labels,{
+    iterations : 100,
+    learningRate : 0.001
+  }
+  )
+
+
+console.log("rate for b is :" + regression.b, "and rate for m is :" + regression.m)
--- regressions/linear-regression.js
+++ regressions/linear-regression.js
@@ -9,22 +9,26 @@ class LinearRegression {
         this.b = 0
     }
 
-    gradientDecent(){
+    gradientDescent(){
         const currentGuessesForMPG = this.features.map( row => {
             return this.m * row[0] + this.b
         })
+    
         const bSlope = _.sum(currentGuessesForMPG.map(( guess, i )=> {
             return guess - this.labels[i][0]
         })) * 2 / this.features.length
-        const test = 0
+        
         const mSlope = _.sum(currentGuessesForMPG.map((guess, i)=>{
             return -1 * this.features[i][0] * (this.labels[i][0]- guess )
         })) * 2 / this.features
-     }
+    
+        this.m = this.m - mSlope * this.options.learningRate
+        this.b = this.b - bSlope * this.options.learningRate
+    
+    }
     train(){
-        const { iterations } = this.options 
-        for(let i = 0; i < iterations; i++){
-            this.gradientDecent()
+        for(let i = 0; i < this.options.iterations; i++){
+            this.gradientDescent()
         }
     }
 }
